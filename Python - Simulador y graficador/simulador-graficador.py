43c64128475204a00aafe9dd2748335cce0a95af
Carlos-Delmote <delmote.carlos@gmail.com>
2023-04-12 11:54:01 -0600

    Create simulador-graficador.py
    Inicio del desarrollo del simulador, definiendo variables.

---
+++ Python - Simulador y graficador/simulador-graficador.py
@@ -0,0 +1,25 @@
+#Definiendo variables
+
+#Temperatura al entrar al radiador
+int(w_temperature_in = )
+
+#Temperatura del aire
+int(a_temperature =)
+
+#Velocidad del aire
+int(a_velocity =)
+
+#Densidad del agua
+int)w_density = 970.518)
+
+#Tasa de flujo volumétrico del agua
+int(w_vflowrate =)
+
+#Tasa de flujo de masa del agua
+int(w_mflowrate = w_density*w_vflowrate)
+
+#Water Specific heat
+int(w_cp = 4184)
+
+#Capacidad total del agua
+int(w_heatcapacity = w_cp*w_mflowrate)
