e4c96e83503c86799be52ac8844d8bacfb0d035c
Carlos-Delmote <delmote.carlos@gmail.com>
2023-04-12 22:49:31 -0600

    Prueba de SQL
    Agregué la parte de SQL y verifiqué funciones básicas de interpretación de datos con csv

--- Python - Simulador y graficador/simulador-graficador.py
+++ Python - Simulador y graficador/simulador-graficador.py
@@ -1,25 +1,3 @@
-#Definiendo variables
-
-#Temperatura al entrar al radiador
-int(w_temperature_in = )
-
-#Temperatura del aire
-int(a_temperature =)
-
-#Velocidad del aire
-int(a_velocity =)
-
-#Densidad del agua
-int)w_density = 970.518)
-
-#Tasa de flujo volumétrico del agua
-int(w_vflowrate =)
-
-#Tasa de flujo de masa del agua
-int(w_mflowrate = w_density*w_vflowrate)
-
-#Water Specific heat
-int(w_cp = 4184)
-
-#Capacidad total del agua
-int(w_heatcapacity = w_cp*w_mflowrate)
+import pandas as pd
+df = pd.read_csv('TEST1.csv')
+print(df.head())
