8e36e89ef3cb9cac702be3edcd33e41b4fc560a9
Carlos-Delmote <delmote.carlos@gmail.com>
2023-04-13 00:45:28 -0600

    Diseño de las gráficas
    Con una base de datos hipotética (TEST2) se realizaron pruebas que funcionarán para hacer el modelo real con los números experimentales mañana.

--- Python - Simulador y graficador/simulador-graficador.py
+++ Python - Simulador y graficador/simulador-graficador.py
@@ -1,3 +1,26 @@
+#Librerías
 import pandas as pd
-df = pd.read_csv('TEST1.csv')
-print(df.head())
+import matplotlib.pyplot as plt
+
+#Lectura de datos SQL
+data = pd.read_csv('TEST2.csv')
+
+#Diferencial de temperatura
+DTemp = data.HTemp - data.LTemp
+
+#Gráfica de datos experimentales
+x = data.Time
+y = data.Voltage * 100
+#El Coeficiente Seebeck del cobre es 0.0035
+y2 = DTemp * 0.0035 * 100
+z = DTemp
+plt.plot(x, y)
+plt.plot(x, z)
+#Valor teórico
+plt.plot(x,y2)
+plt.title("Al amanecer")
+plt.xlabel("Time")
+plt.ylabel("Voltaje en V x 10^2 \n" + "Temperatura en °C")
+plt.legend(["Voltaje (Vx10^2)", "Diferencial de temperatura (°C)", "Voltaje teórico (Vx10^2)"])
+plt.show()
+
