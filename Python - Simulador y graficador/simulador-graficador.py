0c3280bbb6ef25e0800574bf436a2643910926e8
Carlos-Delmote <delmote.carlos@gmail.com>
2023-04-13 12:40:13 -0600

    Pruebas
    Se hizo la primer prueba del prototipo y se alteraron las gráficas para coincidir con lo que se necesitaba.
    Se agregaron Measurements1.csv y Seececk2.bak como exportación de los datos de SQL y backup por si acaso

--- Python - Simulador y graficador/simulador-graficador.py
+++ Python - Simulador y graficador/simulador-graficador.py
@@ -3,14 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #Lectura de datos SQL
-data = pd.read_csv('TEST2.csv')
-
+data = pd.read_csv('Measurements1.csv')
+#Delimitar la gráfica a las pruebas reales e ignorar el resto de datos usado para pruebas
+Idata = data[6 < data.index]
+Pdata = Idata[ Idata.index < 62 ]
 #Diferencial de temperatura
-DTemp = data.HTemp - data.LTemp
+DTemp = Pdata.HTemp - Pdata.LTemp
 
 #Gráfica de datos experimentales
-x = data.Time
-y = data.Voltage * 100
+x = Pdata.Time
+y = Pdata.Voltage * 100
 #El Coeficiente Seebeck del cobre es 0.0035
 y2 = DTemp * 0.0035 * 100
 z = DTemp
@@ -24,3 +26,8 @@ plt.ylabel("Voltaje en V x 10^2 \n" + "Temperatura en °C")
 plt.legend(["Voltaje (Vx10^2)", "Diferencial de temperatura (°C)", "Voltaje teórico (Vx10^2)"])
 plt.show()
 
+#Notas
+#Test 1 = Prueba con datos falsos
+#Test 2 = Primeras mediciones en clime nublado
+#Test 3 = Nube densa
+#Test 100 = Celda Peltier sin termosifón ni parábola
